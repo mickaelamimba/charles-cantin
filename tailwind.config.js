873eed63f1e435d55dafd714e0e36a1f891a2610
AMIMBA Mickael <mickael.amimba@gmail.com>
2021-11-15 20:54:00 -0300

    tailwind fille

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary:{
+          main: '#37444c',
+          hover:'#456d82',
+          focus:'#255d7b',
+          text:'#8ec0dc',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
